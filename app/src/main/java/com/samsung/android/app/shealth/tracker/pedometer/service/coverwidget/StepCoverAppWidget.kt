cce97afcb8dad78c1ae568315ea21bec109002f0
Abandoned Cart <twistedumbrella@gmail.com>
2021-09-04 10:06:42 -0400

    [WIP] Z Flip 3 Cover Screen Launch widget

--- app/src/main/java/com/samsung/android/app/shealth/tracker/pedometer/service/coverwidget/StepCoverAppWidget.kt
+++ app/src/main/java/com/samsung/android/app/shealth/tracker/pedometer/service/coverwidget/StepCoverAppWidget.kt
@@ -1,36 +1,40 @@
 package com.samsung.android.app.shealth.tracker.pedometer.service.coverwidget
 
+import android.app.ActivityOptions
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.widget.RemoteViews
 import com.sec.android.app.shealth.R
 
 
 class StepCoverAppWidget: AppWidgetProvider() {
 
-    private val shealth = "com.sec.android.app.shealth"
-
     private val onClickTag = "OnClickTag"
 
-    protected fun getPendingSelfIntent(context: Context?, action: String?): PendingIntent? {
-        val intent = Intent(context, StepCoverAppWidget()::class.java)
-        intent.action = action
-        return PendingIntent.getBroadcast(context, 0, intent, 0)
-    }
-
     override fun onReceive(context: Context?, intent: Intent) {
-        if (onClickTag.equals(intent.action)) {
-            val intent = Intent()
-            intent.component = ComponentName(shealth, "$shealth.AppLauncherService")
-            context?.startService(intent)
+        if (intent.action == AppWidgetManager.ACTION_APPWIDGET_UPDATE) {
+            // refresh all your widgets
+            val mgr = AppWidgetManager.getInstance(context)
+            val cn = context?.let { ComponentName(it, StepCoverAppWidget::class.java) }
+            mgr.notifyAppWidgetViewDataChanged(mgr.getAppWidgetIds(cn), R.id.widgetListView)
         }
-        else{
-            super.onReceive(context, intent)
+        if (intent.action.equals(onClickTag)) {
+            val launchPackage = intent.getStringExtra("launchPackage")
+            val launchActivity = intent.getStringExtra("launchActivity")
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_LAUNCHER)
+            intent.component = ComponentName(launchPackage!!, launchActivity!!)
+            val options = ActivityOptions.makeBasic().setLaunchDisplayId(1)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            context?.startActivity(intent, options.toBundle())
         }
+        super.onReceive(context, intent);
     }
 
     override fun onUpdate(
@@ -41,10 +45,21 @@ class StepCoverAppWidget: AppWidgetProvider() {
         appWidgetIds?.forEach { appWidgetId ->
             val views = RemoteViews(
                 context?.packageName,
-                R.layout.cover_widget_layout
-            ).apply{
-                this.setOnClickPendingIntent(R.id.launch, getPendingSelfIntent(context, onClickTag))
-            }
+                R.layout.step_widget_view
+            )
+            val intent = Intent(context, StepWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME));
+            views.setRemoteAdapter(R.id.widgetListView, intent)
+
+            val itemIntent = Intent(context, StepCoverAppWidget::class.java)
+            itemIntent.action = onClickTag
+            itemIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            val toastPendingIntent = PendingIntent.getBroadcast(
+                context, 0, itemIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            views.setPendingIntentTemplate(R.id.widgetListView, toastPendingIntent)
 
             appWidgetManager?.updateAppWidget(appWidgetId, views)
 
--- app/src/main/java/com/sec/android/app/shealth/MainActivity.kt
+++ app/src/main/java/com/sec/android/app/shealth/MainActivity.kt
@@ -4,36 +4,47 @@ import android.app.ActivityOptions
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
-    private val defaultUrl = "https://forum.xda-developers.com/f/samsung-galaxy-z-flip-3.12351/"
+
+    private val launchPackage = "org.thoughtcrime.securesms"
+    private val launchActivity = "$launchPackage.RoutingActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sharedPref = getSharedPreferences("com.carudibu.subuibrowser.PREFS", Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences("com.endgames.zflip.launcher.PREFS", Context.MODE_PRIVATE)
 
-        findViewById<EditText>(R.id.startingUrl).setText(sharedPref.getString("starting_url", defaultUrl) ?: defaultUrl)
+        findViewById<EditText>(R.id.launchPackage).setText(sharedPref.getString("launchPackage", launchPackage) ?: launchPackage)
+        findViewById<EditText>(R.id.launchActivity).setText(sharedPref.getString("launchActivity", launchActivity) ?: launchActivity)
 
-        findViewById<EditText>(R.id.startingUrl).doOnTextChanged { text, start, before, count ->
+        findViewById<EditText>(R.id.launchPackage).doOnTextChanged { text, start, before, count ->
             with (sharedPref.edit()) {
-                putString("starting_url", text.toString())
+                putString("launchPackage", text.toString())
+                apply()
+            }
+        }
+        findViewById<EditText>(R.id.launchActivity).doOnTextChanged { text, start, before, count ->
+            with (sharedPref.edit()) {
+                putString("launchActivity", text.toString())
                 apply()
             }
         }
 
-        findViewById<Button>(R.id.openBrowser).setOnClickListener {
-            val intent = Intent(applicationContext, BrowserActivity::class.java)
-            val options = ActivityOptions.makeBasic().setLaunchDisplayId(0)
+        findViewById<Button>(R.id.launchIntent).setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.component = ComponentName(
+                findViewById<EditText>(R.id.launchPackage).text.toString(),
+                findViewById<EditText>(R.id.launchActivity).text.toString()
+            )
+            val options = ActivityOptions.makeBasic().setLaunchDisplayId(1)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
             startActivity(intent, options.toBundle())
         }
     }
